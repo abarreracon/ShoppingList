9f7fbefe038cf3494cdb3ea0d47eeceab44bf623
abarrera <abarreracon@gmail.com>
2020-05-07 16:35:05 -0500

    User can see the current shopping list

--- shopping_list.py
+++ shopping_list.py
@@ -1,19 +1,25 @@
-#Create a new empty list named shopping_list
 shopping_list = []
 
-#Create a function named add_to_list that declares a parameter named item
-def add_to_list(item):
-    #Add the item to the list
-    shopping_list.append(item)
-    #Notify user that the item was added, and state the number of itemes in the list currently
-    print("Added: List has {} items.".format(len(shopping_list)))
 
 def show_help():
     print("What should we pick up at the store?")
     print("""
     Enter 'DONE' to stop adding items.
     ENTER 'HELP' for this help.
+    ENTER 'SHOW' for see your current Shopping list.
     """)
+
+
+def add_to_list(item):
+    shopping_list.append(item)
+    print("Added: List has {} items.".format(len(shopping_list)))
+
+
+def show_list():
+    print("Her's your list: ")
+    for item in shopping_list:
+        print(item)
+
 
 show_help()
 while True:
@@ -24,6 +30,10 @@ while True:
     elif new_item == 'HELP':
         show_help()
         continue
+    elif new_item == 'SHOW':
+        show_list()
+        continue
 
-    #Call add_to_list with new_item as argument
     add_to_list(new_item)
+
+show_list()
