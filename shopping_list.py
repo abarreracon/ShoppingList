de31136ae8fa5692ed3c83be28217a900ce55ba2
abarrera <abarreracon@gmail.com>
2020-05-07 16:21:40 -0500

    User can add items to shopping list, new message is printed with the numbers of items added

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,13 @@
+#Create a new empty list named shopping_list
+shopping_list = []
+
+#Create a function named add_to_list that declares a parameter named item
+def add_to_list(item):
+    #Add the item to the list
+    shopping_list.append(item)
+    #Notify user that the item was added, and state the number of itemes in the list currently
+    print("Added: List has {} items.".format(len(shopping_list)))
+
 def show_help():
     print("What should we pick up at the store?")
     print("""
@@ -14,3 +24,6 @@ while True:
     elif new_item == 'HELP':
         show_help()
         continue
+
+    #Call add_to_list with new_item as argument
+    add_to_list(new_item)
